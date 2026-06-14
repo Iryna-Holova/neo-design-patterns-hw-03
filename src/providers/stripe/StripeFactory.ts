694420f108ad43e8b774bde40b5fa503f0421980
Iryna Holova <gis.golova@gmail.com>
2026-06-14 23:54:05 +0300

    Fix indentation in StripeFactory class

--- src/providers/stripe/StripeFactory.ts
+++ src/providers/stripe/StripeFactory.ts
@@ -2,7 +2,7 @@ import { PaymentProviderFactory } from '../../core/PaymentProviderFactory';
 import { StripePaymentProvider } from './StripePaymentProvider';
 
 export class StripeFactory implements PaymentProviderFactory {
-	createPaymentProvider() {
-		return new StripePaymentProvider();
-	}
+  createPaymentProvider() {
+    return new StripePaymentProvider();
+  }
 }
